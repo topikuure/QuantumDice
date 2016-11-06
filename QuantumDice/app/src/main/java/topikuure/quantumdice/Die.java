17d46c9f6193ae15eb3fc05611f70c08604c3104
topikuure <topikuure@users.noreply.github.com>
2016-11-06 12:04:22 +0200

    Die-luokka ja sen metodit lisätty (noppa)
    Metodeja ei vielä implementoitu

---
+++ QuantumDice/app/src/main/java/topikuure/quantumdice/Die.java
@@ -0,0 +1,17 @@
+package topikuure.quantumdice;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by Topi on 06/11/2016.
+ */
+public class Die {
+
+    public int sides = 6;
+
+    public void roll() {
+    }
+
+    public void draw(Canvas canvas) {
+    }
+}
