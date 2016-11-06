54e9eca87e975950c41d9893a19fbc169f4e6d7c
topikuure <topikuure@users.noreply.github.com>
2016-11-06 13:41:31 +0200

    QuantumRandom.getRandomNumber-metodi toteutettu

--- QuantumDice/app/src/main/java/topikuure/quantumdice/QuantumRandom.java
+++ QuantumDice/app/src/main/java/topikuure/quantumdice/QuantumRandom.java
@@ -2,12 +2,25 @@ package topikuure.quantumdice;
 
 /**
  * Created by Topi on 06/11/2016.
+ *
+ * QuantumRandom käyttää QRNG@ANU JSON API -rajapintaa
+ * https://qrng.anu.edu.au/API/api-demo.php
  */
 public class QuantumRandom {
 
-    private int testi = 0;
+    private int bufferTopIndex = 0;
+    private int buffer[] = {48,223,28,238,228,72,151,179,168,2};
+    private String arrayLength = "10";
 
-    public int getRandomNumber(int min, int max) {//TODO implementoi
-        return testi++;
+    public int getRandomNumber(int min, int max) {
+        if(bufferTopIndex >= buffer.length) fillBuffer();
+        return (buffer[bufferTopIndex++] % (max - min + 1)) + min;
+    }
+
+    private boolean fillBuffer() {//TODO implementoi
+        //String url = "https://qrng.anu.edu.au/API/jsonI.php?length=" + arrayLength + "&type=uint8";
+        //Hae bufferiin data netistä
+        bufferTopIndex = 0;
+        return false;
     }
 }
